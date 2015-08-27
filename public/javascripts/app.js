371e7d01d27af761e8a351aa7f8b5de075c4b31b
Anna Casper <anna.casper3@gmail.com>
2015-08-27 11:23:41 -0600

    working on partials

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,9 @@
-var app = angular.module('posts-workshop', []);
+var app = angular.module('posts-workshop', ['ngRoute']);
 
 app.controller('ApplicationController', function ($scope) {
   $scope.test = 'test';
+  config(['$routeProvider', function($routeProvider, ApplicationController) {
+    $routeProvider.when('/', {templateUrl: 'partials/posts.html', controller: 'ApplicationController'});
+    $routeProvider.otherwise({redirectTo: '/'});
+  }]);
 })

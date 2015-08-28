622bfa5b042af1f01df84dcda2ca3e46272fc6ad
Anna Casper <anna.casper3@gmail.com>
2015-08-28 10:30:52 -0600

    routing is working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,8 +2,12 @@ var app = angular.module('posts-workshop', ['ngRoute']);
 
 app.controller('ApplicationController', function ($scope) {
   $scope.test = 'test';
-  config(['$routeProvider', function($routeProvider, ApplicationController) {
-    $routeProvider.when('/', {templateUrl: 'partials/posts.html', controller: 'ApplicationController'});
-    $routeProvider.otherwise({redirectTo: '/'});
-  }]);
 })
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/posts.html',
+    controller: 'ApplicationController'
+  })
+}]);
